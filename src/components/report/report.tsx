bff62165a70434ed520966c8c6b0fe19d777a6e6
Anna Iustus <ajustusa@gmail.com>
2023-06-27 10:24:06 +0200

    feat: add assets

--- src/components/report/report.tsx
+++ src/components/report/report.tsx
@@ -1,6 +1,11 @@
-import { CloseOutlined } from '@ant-design/icons';
 import CollapsedBlock from '../collapsedBlock/collapsedBlock';
 import { Button } from 'antd';
+import { getId, getDataType, getDataValue } from '../../utils';
+import { AppDispatch } from '../../store/store';
+import { useDispatch } from 'react-redux';
+import { addData } from '../../store/reportSlice';
+
+import { CloseOutlined } from '@ant-design/icons';
 import styles from './report.module.scss';
 
 export type TData = {
@@ -16,6 +21,11 @@ export interface IReport {
 
 export type ReportsHash = { [key: string]: IReport };
 
+export interface IReportData {
+  reportId: string;
+  newData: TData;
+}
+
 interface ReportProps {
   report: IReport;
   onDeleteReport: () => void;
@@ -24,21 +34,43 @@ interface ReportProps {
 
 const Report = ({ report, onDeleteReport, onDeleteData }: ReportProps) => {
   const { id, data } = report;
+  const dispatch: AppDispatch = useDispatch<AppDispatch>();
+
+  const createNewData = () => {
+    const dataId = getId();
+    const dataType = getDataType();
+    const dataValue = getDataValue();
+    const newDataItem: TData = {
+      id: `d-${dataId}`,
+      type: dataType,
+      value: dataValue,
+    };
+    dispatch(addData({ reportId: id, newData: newDataItem }));
+  };
+
   return (
     <CollapsedBlock label={`Report #${id}`} onDelete={onDeleteReport}>
-      {data.map((dataItem) => (
-        <div key={dataItem.id} className={styles.datum}>
-          <Button
-            type="primary"
-            shape="circle"
-            size="small"
-            icon={<CloseOutlined />}
-            onClick={() => onDeleteData(dataItem.id, id)}
-            className={styles['close-btn']}
-          />
-          {dataItem.value}
-        </div>
-      ))}
+      <p>Report #{id} data</p>
+      <Button type="primary" onClick={createNewData}>
+        Add data
+      </Button>
+      <div className={styles.dataItems}>
+        {data.map((dataItem) => (
+          <div key={dataItem.id} className={styles.dataItem}>
+            <Button
+              type="primary"
+              shape="circle"
+              size="small"
+              icon={<CloseOutlined />}
+              onClick={() => onDeleteData(dataItem.id, id)}
+              className={styles['close-btn']}
+            />
+            <div className={styles.dataView}>
+              <img src={`/img/${dataItem.value}`}></img>
+            </div>
+          </div>
+        ))}
+      </div>
     </CollapsedBlock>
   );
 };
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,14 @@
-export {}
+export const dataTypes = ['grid', 'chart', 'picture', 'graphs'];
+
+export const pictures = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+  '10.jpg',
+];
